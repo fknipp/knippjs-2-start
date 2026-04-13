2dbc4899e494efa498221c710466941d43e316c3
fknipp <franz@qnipp.com>
2026-04-13 21:10:07 +0200

    Convert interaction to use jQuery

--- script.js
+++ script.js
@@ -8,40 +8,38 @@ Ausgabe mit alert(), Eingabe mit prompt(), Ja/Nein-Frage mit confirm()
 
 // Zahl ausdenken
 const ausgedachteZahl = Math.floor(Math.random() * 100 + 1);
-let eingegebeneZahl;
 
-do {
+$("#form").on("submit", (e) => {
+
+  e.preventDefault();
+
   // Frage: Überlege eine Zahl von 1 bis 100.
   // Person gibt Antwort ein
-  const eingabe = prompt("Überlege eine Zahl von 1 bis 100.");
+  const eingabe = $("#form input").val();
 
-  if (eingabe === null) {
-    break;
-  }
-
-  eingegebeneZahl = parseInt(eingabe);
+  const eingegebeneZahl = parseInt(eingabe);
 
   if (isNaN(eingegebeneZahl)) {
-    alert("Die Eingabe ist ungültig.");
-    continue;
+    $(".output").text("Die Eingabe ist ungültig.");
+    return;
   }
 
   // Vergleich: eingegebene Zahl größer als die ausgedachte Zahl
   // Ausgabe: Meine Zahl ist kleiner
   if (eingegebeneZahl > ausgedachteZahl) {
-    alert("Meine Zahl ist kleiner.");
+    $(".output").text("Meine Zahl ist kleiner.");
   }
 
   // Sonst Vergleich: eingebene Zahl ist kleiner als die ausgedachte Zahl
   // Ausgabe: Meine Zahl ist größer
   else if (eingegebeneZahl < ausgedachteZahl) {
-    alert("Meine Zahl ist größer.");
+    $(".output").text("Meine Zahl ist größer.");
   }
 
   // Sonst Ausgabe: Du hast die Zahl erraten.
   else {
-    alert("Du hast die Zahl erraten.");
+    $(".output").text("Du hast die Zahl erraten.");
   }
 
   // Neue Antwort eingeben (Sprung zurück, wenn die eingegebene Zahl ungleich der ausgedachten Zahl ist)
-} while (eingegebeneZahl !== ausgedachteZahl);
+});
