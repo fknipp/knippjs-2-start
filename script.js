164ba841ed31ec7e4c62999ce6b94fe0176f42a7
fknipp <franz@qnipp.com>
2026-08-20 11:52:35 +0200

    Version using jQuery

--- script.js
+++ script.js
@@ -7,24 +7,24 @@ Ausgabe mit alert(), Eingabe mit prompt(), Ja/Nein-Frage mit confirm()
 */
 
 const generatedNumber = Math.floor(Math.random() * 100 + 1);
-
 console.log(generatedNumber);
 
-while (true) {
-  const inputString = prompt("Guess a number between 1 and 100");
-  if (inputString === null) {
-    break;
-  }
+$("#form").on("submit", (e) => {
+  e.preventDefault();
+  console.log(e);
+
+  const inputString = $("#form input").val();
   const inputNumber = parseInt(inputString);
 
   if (inputNumber > generatedNumber) {
-    alert("The searched number is smaller.");
+    $(".output").text("The searched number is smaller.");
   } else if (inputNumber < generatedNumber) {
-    alert("The searched number is bigger.");
+    $(".output").text("The searched number is bigger.");
   } else if (inputNumber === generatedNumber) {
-    alert("You found the correct number.");
-    break;
+    $(".output").text("You found the correct number.");
   } else {
-    alert("Your input was not a valid number.");
+    $(".output").text("Your input was not a valid number.");
   }
-}
+
+  $("#form input").focus();
+});
